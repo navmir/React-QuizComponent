4ffec4dc9ca1350c2e65e5eb550fde2aaf925b7b
Navid <navid.mir-babaei@ffcg.se>
2019-01-25 09:58:14 +0100

    added instr text

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,7 +12,7 @@ class Quiz extends Component {
 	   return (
 		<div>
 		  <div className="QuizQuestion">
-		    Hej
+		    {quizData.quiz_questions[0].instruction_text} 
 		  </div>
 		</div>
 		);
