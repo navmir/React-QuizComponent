a51bae661cbad384a10d5f6711fb8caf9e469893
Navid <navid.mir-babaei@ffcg.se>
2019-01-25 10:22:29 +0100

    2:8 & 9

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,11 +5,13 @@ class QuizQuestion extends Component {
 		return (
 			<main>
 			<section>
-				<p>//Instruction goes here</p>
+				<p>{this.props.instruction_text}</p>
 			</section>
 			<section className="buttons">
 				<ul>
-				//quiz question button logic goes here
+				  <li>
+					{this.props.quiz_question.answer_options[0]}
+				  </li>
 				</ul>
 			</section>
 			</main>
